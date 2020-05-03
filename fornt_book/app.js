aa8b3b8eefa11e4dcd3aab42f0092d8d7b97e00f
gnsdlwkd95 <50192371+gnsdlwkd95@users.noreply.github.com>
2020-05-03 18:43:36 +0900

    05_03

--- fornt_book/app.js
+++ fornt_book/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+let methodOverride = require("method-override")
 const app = express();
+
+app.set('view engine', 'ejs'); // 템플릿 엔진 설정(필수)
+app.set('views', __dirname + '/view'); // 템플릿 파일 위치 설정(필수)
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 const movieRouter = require('./router/BookRouter');
 app.use(movieRouter);
---
+++ fornt_book/model/BookModel.js
@@ -0,0 +1,137 @@
+const fs = require('fs');
+
+class Book {
+    constructor() {
+        const data = fs.readFileSync('./model/data.json');
+        this.books = JSON.parse(data)
+    }
+
+    putUpdateBook(index, title, writer, year, summary) {        
+        return new Promise((resolve, reject) => {
+
+            let id = Number(index);
+            let newBook = {index, title, writer, year, summary};            
+            for (var book of this.books ) {
+                if ( book.index == id ) {
+                    this.books.splice(index, 1, newBook); 
+                    // index번의 내용 1개 삭제 후 newBook 내용 새로 추가
+                    resolve(newBook);
+                    return;
+                }
+            }
+        });
+    }
+
+    // Promise 예제
+    getBookList() {
+        if (this.books) {
+            return this.books;
+        }
+        else {
+            return [];
+        }
+    }
+
+    deleteBookData(bookIndex) {
+        return new Promise((resolve, reject) => {
+            for (var book of this.books ) {
+                if ( book.index == bookIndex ) {
+                    this.books.splice(bookIndex, 1);
+                    resolve(book);
+                    return;
+                }
+            }
+            reject({msg:'Can not find book', code:404});
+        });
+    }
+
+    addBook(title, writer, year, summary) {
+        return new Promise((resolve, reject) => {
+            let last = this.books[this.books.length - 1];
+            let index = last.index + 1;
+
+            let newBook = {index, title, writer, year, summary};
+            this.books.push(newBook);
+
+            resolve(newBook);
+        });
+    }
+
+    // Promise - Reject
+    getBookDetail(bookIndex) {
+        return new Promise((resolve, reject) => {
+            for (var book of this.books ) {
+                if ( book.index == bookIndex ) {                
+                    resolve(book);
+                    return;
+                }
+            }
+            reject({msg:'Can not find book', code:404});
+        });
+    }
+
+    getBookSearch(title, writer, year) {
+        return new Promise((resolve, reject) => {
+            if(title && !writer && !year){
+                for (var book of this.books ) {
+                    if ( book.title == title ) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+            else if(!title && writer && !year){
+                for (var book of this.books ) {
+                    if ( book.writer == writer ) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+            else if(!title && !writer && year){
+                for (var book of this.books ) {
+                    if ( book.year == year ) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+            else if(!title && writer && year){
+                for (var book of this.books ) {
+                    if ( book.year == year && book.writer == writer) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+            else if(title && !writer && year){
+                for (var book of this.books ) {
+                    if ( book.year == year && book.title == title) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+            else if(title && writer && !year){
+                for (var book of this.books ) {
+                    if ( book.title == title && book.writer == writer) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+            else if(title && writer && year){
+                for (var book of this.books ) {
+                    if ( book.year == year && book.title == title && book.writer == writer ) {
+                        resolve(book);
+                        return;
+                    }
+                }
+            }
+
+            reject({msg:'Can not find book', code:404});            
+        });
+    }
+}
+
+module.exports = new Book();
